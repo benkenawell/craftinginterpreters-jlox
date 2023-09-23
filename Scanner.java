5ecabbad1891e9c52c13b1b5fa3893199b98369f
Ben Kenawell <ben@benkenawell.com>
2023-09-23 15:52:44 -0400

    4.5.2 operators, add one or two character operators to our big switch statement

--- Scanner.java
+++ Scanner.java
@@ -42,6 +42,18 @@ class Scanner {
       case '+': addToken(PLUS); break;
       case ';': addToken(SEMICOLON); break;
       case '*': addToken(STAR); break;
+      case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+      case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+      case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+      case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
       default:
         Lox.error(line, "Unexpected character.");
         break;
@@ -54,6 +66,13 @@ class Scanner {
 
   private char advance() {
     return source.charAt(current++);
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+    current++;
+    return true;
   }
 
   private void addToken(TokenType type) {
